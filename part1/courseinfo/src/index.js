ee9bb6d6483978f6719865daebcea694506b76b6
Jason <jasylwong@gmail.com>
2020-05-02 13:16:55 +0100

    add Part component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -20,9 +20,7 @@ const Content = (props) => (
 )
 
 const Total = (props) => (
-  <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  </>
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 )
 
 const App = () => {
