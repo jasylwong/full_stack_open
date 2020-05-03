ca4fd162c643531a740d5d77d7bad80c3086482c
Jason <jasylwong@gmail.com>
2020-05-03 21:23:31 +0100

    complete ex2.4

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -7,12 +7,12 @@ const Header = ({ course }) => {
   )
 }
 
-// const Total = ({ course }) => {
-//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-//   return(
-//     <p>Number of exercises {sum}</p>
-//   ) 
-// }
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((a, b) => a + b.exercises, 0)
+  return(
+    <p><strong>Total of {sum} exercises</strong></p>
+  ) 
+}
 
 const Part = (props) => {
   return (
@@ -37,6 +37,7 @@ const Course = ({ course }) => {
     <>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
@@ -60,6 +61,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
