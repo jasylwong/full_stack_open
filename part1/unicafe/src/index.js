c849e88c75d31d7c0feb2a67da289a7b6f5f6636
Jason <jasylwong@gmail.com>
2020-05-03 12:33:22 +0100

    add message when no feedback yet

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,7 @@ const Button = ({addCount, text}) => (
   <button onClick={addCount}>{text}</button>
 )
 
-const Display = ({option, optionCount}) => <div>{option} {optionCount}</div>
+const Statistic = ({text, stat}) => <div>{text} {stat}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -18,6 +18,27 @@ const App = () => {
   const addNeutral = () => setNeutral(neutral + 1)
   const addBad = () => setBad(bad + 1)
 
+  let all = good + neutral + bad
+  let average = all === 0 ? 0 : (good + (bad * -1))/ all
+  let positive = all === 0 ? '0%' : `${(good / all) * 100}%` 
+
+  let stats = () => {
+    if (all !== 0 ) {
+      return (
+        <>
+        <Statistic text={"good"} stat={good} />
+        <Statistic text={"neutral"} stat={neutral} />
+        <Statistic text={"bad"} stat={bad} />
+        <Statistic text={"all"} stat={all} />
+        <Statistic text={"average"} stat={average} />
+        <Statistic text={"positive"} stat={positive} />
+        </>
+      )
+    } else {
+      return 'No feedback given'
+    }
+  }
+
   return (
     <>
       <Heading text={'Give feedback'} />
@@ -25,9 +46,7 @@ const App = () => {
       <Button addCount={addNeutral} text={'Neutral'} />
       <Button addCount={addBad} text={'Bad'} />
       <Heading text={'statistics'} />
-      <Display option={"good"} optionCount={good} />
-      <Display option={"neutral"} optionCount={neutral} />
-      <Display option={"bad"} optionCount={bad} />
+      {stats()}
     </>
   )
 }
