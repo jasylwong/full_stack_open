28271161613604fdd35ef09a94b13de2fe61b4af
Jason <jasylwong@gmail.com>
2020-05-04 10:20:01 +0100

    add button for toggling show all/important

--- part2/rendering/src/App.js
+++ part2/rendering/src/App.js
@@ -4,16 +4,17 @@ import Note from './components/Note';
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
-    const noteObject = {
+    const newNoteDetails = {
+      id: notes.length + 1,
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: true
     }
-    setNotes(notes.concat(noteObject))
+    setNotes(notes.concat(newNoteDetails))
     setNewNote('')
   }
 
@@ -22,17 +23,25 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>  
+      <div>
+        <button onClick={() => setShowAll(!showAll)} >
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
-      <form onSubmit={addNote} >
+      <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
-        <button type='submit'>save</button>
+        <button value='submit'>save</button>
       </form>
-
     </div>
   )
 }
