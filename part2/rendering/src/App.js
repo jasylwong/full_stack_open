54e305dbe8f7199abc594946e0a153d09c77feb3
Jason <jasylwong@gmail.com>
2020-05-04 09:37:18 +0100

    complete addNote event handler function

--- part2/rendering/src/App.js
+++ part2/rendering/src/App.js
@@ -3,12 +3,23 @@ import Note from './components/Note';
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
-
+  const [newNote, setNewNote] = useState('')
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
   return (
@@ -17,10 +28,11 @@ const App = (props) => {
       <ul>
         {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
-      <form onSubmit={addNote}>
-        <input />
+      <form onSubmit={addNote} >
+        <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+
     </div>
   )
 }
