ff658a486a71107a6a71306d38f0c8ee0872eb66
Jason <jasylwong@gmail.com>
2020-05-04 12:20:44 +0100

    remove unnecessary modules

--- part2/phonebook/src/components/Form.js
+++ part2/phonebook/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Form = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) => {
   return (
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,67 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Filter from './components/Filter';
-import Form from './components/Form';
-import Persons from './components/Persons';
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    if (persons.map(person => person.name).includes(newName)) {
-      window.alert(`${newName} is already added to the phonebook`)
-    } else if (persons.map(person => person.number).includes(newNumber)) {
-      window.alert(`${newNumber} is already used by someone else`)
-    } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilter = (event) => {
-    setFilter(event.target.value)
-  }
-
-  const filteredPersons = 
-    ( filter === ''
-      ? persons
-      : persons.filter(person => person.name.toLowerCase().includes(filter))
-    )
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilter={handleFilter}/>
-      <h2>Add a new</h2>
-      <Form addPerson={addPerson}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
-      <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons} persons={persons}/>
-    </div>
-  )
-}
+import App from './App';
 
 ReactDOM.render(
     <App />,
