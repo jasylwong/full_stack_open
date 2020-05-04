1a66a1ae36ad0cacc8df95a7ca2bed7135aafd0f
Jason <jasylwong@gmail.com>
2020-05-04 11:10:23 +0100

    complete ex2.8

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -2,21 +2,29 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '123'}])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.map(person => person.name).includes(newName)) {
       window.alert(`${newName} is already added to the phonebook`)
+    } else if (persons.map(person => person.number).includes(newNumber)) {
+      window.alert(`${newNumber} is already used by someone else`)
     } else {
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newNumber }))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -27,11 +35,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={persons.indexOf(person)}>{person.name}</div>)}
+      {persons.map(person => 
+        <div key={persons.indexOf(person)}>{person.name} {person.number}</div>)}
     </div>
   )
 }
