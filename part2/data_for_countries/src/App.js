a8a126436310a5fd63109e87b869e1ba63abd969
Jason <jasylwong@gmail.com>
2020-05-05 09:02:08 +0100

    extract Results to own component

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { render } from 'react-dom';
 
-// const Results = ({ countries }) => {
-
-//   return results();
-// }
-
+const Results = ({ countries }) => {
+  if (countries.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  } else if (countries.length > 1) {
+    return countries.map(country => 
+      <div key={countries.indexOf(country)}>{country.name}</div>)
+  } else if (countries.length === 1) {
+    return (
+      <div>
+        <h2>{countries[0].name}</h2>
+        <div>capital {countries[0].capital}</div>
+        <div>population {countries[0].population}</div>
+        <h3>Languages</h3>
+        <ul>
+          {countries[0].languages.map(language => 
+            <li key={countries[0].languages.indexOf(language)}>{
+              language.name}
+            </li>)}
+        </ul>
+        <img src={countries[0].flag} width='100px' alt={countries[0].name}/>
+      </div>
+    )
+  } else {
+    return <div></div>
+  }
+}
 
 const App = () => {
   const [countries, setCountries] = useState([])
@@ -29,33 +51,10 @@ const App = () => {
         country.name.toLowerCase().includes(search.toLowerCase()))
     )
 
-    const results = (countries) => {
-      if (countries.length > 10) {
-        return <div>Too many matches, specify another filter</div>
-      } else if (countries.length > 1) {
-        return countries.map(country => 
-          <div key={countries.indexOf(country)}>{country.name}</div>)
-      } else if (countries.length === 1) {
-        return (
-          <div>
-            <h2>{countries[0].name}</h2>
-            <div>capital {countries[0].capital}</div>
-            <div>population {countries[0].population}</div>
-            <h3>Languages</h3>
-            <ul>
-              {countries[0].languages.map(language => <li key={countries[0].languages.indexOf(language)}>{language.name}</li>)}
-            </ul>
-            <img src={countries[0].flag} width='100px' alt={countries[0].name}/>
-          </div>
-        )
-      }
-    }
-
   return (
     <div>
       find countries <input value={search} onChange={handleSearch} />
-      {/* <Results countries={filteredCountries} /> */}
-      {results(filteredCountries)}
+      <Results countries={filteredCountries} />
     </div>
   )
 }
