17363909411e88ac786ba474d5f6b14401fd45ff
Jason <jasylwong@gmail.com>
2020-05-05 13:52:12 +0100

    complete ex2.13

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { render } from 'react-dom';
 
-const Results = ({ countries }) => {
+const Results = ({ countries, showCountry }) => {
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length > 1) {
-    return countries.map(country => 
-      <div key={countries.indexOf(country)}>{country.name}</div>)
-  } else if (countries.length === 1) {
     return (
-      <div>
-        <h2>{countries[0].name}</h2>
-        <div>capital {countries[0].capital}</div>
-        <div>population {countries[0].population}</div>
-        <h3>Languages</h3>
-        <ul>
-          {countries[0].languages.map(language => 
-            <li key={countries[0].languages.indexOf(language)}>{
-              language.name}
-            </li>)}
-        </ul>
-        <img src={countries[0].flag} width='100px' alt={countries[0].name}/>
-      </div>
+      countries.map(country => 
+        <div key={countries.indexOf(country)}>
+          {country.name}<button onClick={() => showCountry(country)} >show</button>
+        </div>
+      )
     )
+  } else if (countries.length === 1) {
+    return <Country country={countries[0]} />
   } else {
     return <div></div>
   }
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>Languages</h3>
+      <ul>
+        {country.languages.map(language => 
+          <li key={country.languages.indexOf(language)}>{
+            language.name}
+          </li>)}
+      </ul>
+      <img src={country.flag} width='100px' alt={country.name}/>
+    </div>
+  )
 }
 
 const App = () => {
@@ -35,6 +43,10 @@ const App = () => {
   
   const handleSearch = (event) => {
     setSearch(event.target.value)
+  }
+
+  const showCountry = (country) => {
+    setSearch(country.name)
   }
 
   const hook = () => {
@@ -54,7 +66,7 @@ const App = () => {
   return (
     <div>
       find countries <input value={search} onChange={handleSearch} />
-      <Results countries={filteredCountries} />
+      <Results countries={filteredCountries} showCountry={showCountry} />
     </div>
   )
 }
