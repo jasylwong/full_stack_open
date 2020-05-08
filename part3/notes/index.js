1a2080f0f176a1d85e4cda1edd92a95306e81c10
Jason <jasylwong@gmail.com>
2020-05-08 11:47:02 +0100

    add post route

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -56,13 +56,31 @@ app.get('/api/notes/:id', (request, response) => {
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
-
   response.status(204).end()
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = body.request
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
