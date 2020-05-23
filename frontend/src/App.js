388a5d0427ccc3566dfbaca96bc5bf30acd6d6d9
Jason <jasylwong@gmail.com>
2020-05-23 23:24:17 +0100

    add state for setting new note

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,36 @@ import './App.css';
 
 function App(props) {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note')
+
+   const handleSubmit = () => {
+    //  setNotes(notes)
+     console.log('Submitted!')
+   }
+
+   const handleChange = (event) => {
+     console.log(event.target.value)
+     setNewNote(event.target.value)
+   }
 
   return (
     <div className="App">
+      <h2>Notes</h2>
       {
         notes.map(note => {
           return <Note key={note.id} note={note} />
         })
       }
+
+      <h2>Add a note</h2>
+      <form onSubmit={() => handleSubmit()}>
+        <label>
+          Note:
+          <input type='text' name='name' value={newNote} onChange={handleChange}/>
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+      <br />
     </div>
   );
 }
