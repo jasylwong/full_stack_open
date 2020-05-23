54b8ad6ccad9e199992b8229a4dd8ed09f6c175c
Jason <jasylwong@gmail.com>
2020-05-23 22:17:12 +0100

    add button component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Button({ handleClick, word }) {
+  return (
+    <button onClick={() => handleClick(word)} >{word}</button>
+  )
+}
+
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = (word)  => {
+    console.log(word)
+    setCounter(counter + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{counter}</div>
+      {/* <button onClick={handleClick} >left</button>
+      <button onClick={handleClick} >right</button> */}
+      <Button handleClick={handleClick} word={'left'} />
+      <Button handleClick={handleClick} word={'right'} />
     </div>
   );
 }
