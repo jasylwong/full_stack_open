76d35986e43279044c2e74c5116c2dd3206cca2a
Jason <jasylwong@gmail.com>
2020-05-30 11:18:50 +0100

    link importance toggler operation with db

--- backend/index.js
+++ backend/index.js
@@ -37,18 +37,6 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error)) 
 })
 
-const errorHandler = (error, request, response, next) => {
-  console.log(error)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
 // const generateId = () => {
 //   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
 //   return maxId + 1
@@ -75,17 +63,44 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end() 
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).json({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
