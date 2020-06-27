978a9461f516f63d584c4ff22f36ca3c6793c8dd
Jason <jasylwong@gmail.com>
2020-06-27 16:55:56 +0100

    5b - State of the forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ import './App.css';
 
 function App() {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
@@ -32,23 +31,11 @@ function App() {
     }
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5
-    }
-
+  const addNote = (noteObject) => {
     noteService.create(noteObject)
       .then(returnedNote => {
-        setNotes([...notes, returnedNote])
-        setNewNote('')
+        setNotes(notes.concat(returnedNote))
       })
-  }
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
@@ -125,11 +112,7 @@ function App() {
         <div>
           <p>{user.name} logged-in</p>
           <Togglable buttonLabel='new note'>
-            <NoteForm
-              onSubmit={addNote}
-              value={newNote}
-              handleChange={handleNoteChange}
-            />
+            <NoteForm createNote={addNote} />
           </Togglable>
         </div>
       }
